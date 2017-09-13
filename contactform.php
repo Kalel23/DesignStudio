11ea896ca27365d4cfe93a12451be411b626877f
Kalel23 <mishiboy@gmail.com>
2017-09-13 12:28:43 +0530

    modified contact form

--- contactform.php
+++ contactform.php
@@ -4,7 +4,7 @@ $to = 'mishaal@planecrazystudios.com';  //Recipient's or Your E-mail
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'Content-Type: text/html; charset=iso-8859-1' . "\r\n";
 
 $message .= 'First Name: ' . $_REQUEST['fname'] . "<br>";
 $message .= 'Last Name: ' . $_REQUEST['lname'] . "<br>";
