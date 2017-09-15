c5e1257955d248cda7d7da3ff9ac20d497cbc4cb
Kalel23 <mishiboy@gmail.com>
2017-09-16 01:11:34 +0530

    form php edit

--- contact.php
+++ contact.php
@@ -1,25 +1,27 @@
+$email= $_POST['email']; 
+
 <?php
 
 $to = 'mishaal@planecrazystudios.com';  //Recipient's or Your E-mail
-$subject = $_REQUEST['subject'] . ; // Subject of your email
+$subject = $_POST ['subject'] . ; // Subject of your email
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-$message .= 'First Name: ' . $_REQUEST['fname'] . "\r\n";
-$message .= 'Last Name: ' . $_REQUEST['lname'] . "\r\n";
-$message .= 'Subject: ' . $_REQUEST['subject'] . "\r\n";
-$message .= $_REQUEST['message'];
+$message .= 'First Name: ' . $_POST['fname'] . "\r\n";
+$message .= 'Last Name: ' . $_POST['lname'] . "\r\n";
+$message .= 'Subject: ' . $_POST['subject'] . "\r\n";
+$message .= $_POST ['message'];
 
-if (@mail($to, $subject, $message, $headers))
-{
-	// Transfer the value 'sent' to ajax function for showing success message.
-	echo 'sent';
-}
-else
-{
-	// Transfer the value 'failed' to ajax function for showing error message.
-	echo 'failed';
+$send_contact=mail($to,$subject,$message,$header);
+
+// Check, if message sent to your email 
+// display message "We've recived your information"
+if($send_contact){
+    header('Location: confirm.html'); 
+}    
+else {
+echo "ERROR";
 }
 ?>
