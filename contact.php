6981dfe5a9dc16b6f77935808ec9268a5ce3b37a
Kalel23 <mishiboy@gmail.com>
2017-09-16 00:50:43 +0530

    php

--- contact.php
+++ contact.php
@@ -1,14 +1,14 @@
 <?php
-$subject = $_REQUEST['subject'] . ' Urip Landing Page : Demo'; // Subject of your email
+$subject = $_REQUEST['subject'] . ; // Subject of your email
 $to = 'mishaal@planecrazystudios.com';  //Recipient's or Your E-mail
 
-$headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
+$headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-$message .= 'First Name: ' . $_REQUEST['fname'] . "<br>";
-$message .= 'Last Name: ' . $_REQUEST['lname'] . "<br>";
-$message .= 'Subject: ' . $_REQUEST['subject'] . "<br><br><br>";
+$message .= 'First Name: ' . $_REQUEST['fname'] . "\r\n";
+$message .= 'Last Name: ' . $_REQUEST['lname'] . "\r\n";
+$message .= 'Subject: ' . $_REQUEST['subject'] . "\r\n";
 $message .= $_REQUEST['message'];
 
 if (@mail($to, $subject, $message, $headers))
