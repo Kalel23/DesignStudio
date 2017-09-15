0a8b27398a3e61850fcf04db11ef5c8ec0f5d76e
Kalel23 <mishiboy@gmail.com>
2017-09-16 00:56:35 +0530

    code form

--- contact.php
+++ contact.php
@@ -1,9 +1,10 @@
 <?php
-$subject = $_REQUEST['subject'] . ; // Subject of your email
-$to = 'mishaal@planecrazystudios.com';  //Recipient's or Your E-mail
 
-$headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
+$to = 'mishaal@planecrazystudios.com';  //Recipient's or Your E-mail
+$subject = $_REQUEST['subject'] . ; // Subject of your email
+
 $headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 $message .= 'First Name: ' . $_REQUEST['fname'] . "\r\n";
