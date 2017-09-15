8de6499cbf7f822b29caab98766b87cda7d903ec
Kalel23 <mishiboy@gmail.com>
2017-09-16 01:38:24 +0530

    form

--- contactform.php
+++ contactform.php
@@ -8,7 +8,7 @@ $message .= 'Subject: ' . $_POST['subject'] . "<br><br><br>";
 $message .= $_POST['message'];
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
+$headers .= "From: " . $_POST['email'] . "\r\n"; // Sender's E-mail
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 if (mail($to, $subject, $message, $headers))
