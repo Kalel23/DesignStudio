b3391069e87a09d2b87b6883c432b7eec5b69d7c
Kalel23 <mishiboy@gmail.com>
2017-09-16 13:15:55 +0530

    REQUEST

--- contactform.php
+++ contactform.php
@@ -2,16 +2,16 @@
 $to = 'mishaalnathani@outlook.com';  //Recipient's or Your E-mail
 $subject = "Contact Form Query"; // Subject of your email
 
-$message .= 'First Name: ' . $_POST['fname'] . "<br>";
-$message .= 'Last Name: ' . $_POST['lname'] . "<br>";
-$message .= 'Subject: ' . $_POST['subject'] . "<br><br><br>";
-$message .= $_POST['message'];
+$message .= 'First Name: ' . $_REQUEST['fname'] . "<br>";
+$message .= 'Last Name: ' . $_REQUEST['lname'] . "<br>";
+$message .= 'Subject: ' . $_REQUEST['subject'] . "<br><br><br>";
+$message .= $_REQUEST['message'];
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= "From: " . $_POST['email'] . "\r\n"; // Sender's E-mail
+$headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-if (mail($to, $subject, $message, $headers))
+if (@mail($to, $subject, $message, $headers))
 {
 	// Transfer the value 'sent' to ajax function for showing success message.
 	echo 'sent';
